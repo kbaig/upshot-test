377d857666f2c3db2d0a608c0fdfabf3f256b83e
Kaz Baig <khizermbaig@gmail.com>
2019-06-20 17:30:37 -0500

    responsive navigation

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -5,21 +5,37 @@ import data from '../../data/nav';
 
 import logo from '../../assets/logo.svg';
 
-const Nav = ({ ...rest }) => (
-  <nav className='Nav' {...rest}>
-    <a className='Nav__Logo' href='/'>
-      <img src={logo} alt='home' className='Nav__LogoImage' />
-    </a>
-    <ul className='Nav__List'>
-      {data.map(({ content, path }, i) => (
-        <li key={i} className='Nav__ListItem'>
-          <a href={path} className='Nav__ListItemLink'>
-            {content}
-          </a>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Nav = ({ ...rest }) => {
+  const [expanded, setExpanded] = React.useState(true);
+
+  return (
+    <>
+      <nav className={`Nav${expanded ? ' Nav--expanded' : ''}`} {...rest}>
+        <a className='Nav__Logo' href='/'>
+          <img src={logo} alt='home' className='Nav__LogoImage' />
+        </a>
+        <ul className='Nav__List'>
+          {data.map(({ content, path }, i) => (
+            <li key={i} className='Nav__ListItem'>
+              <a href={path} className='Nav__ListItemLink'>
+                {content}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <button
+          className='Nav__HamburgerButton'
+          onClick={() => setExpanded(!expanded)}
+        >
+          <div className='Nav__HamburgerIcon' />
+        </button>
+      </nav>
+      <div
+        className='Nav__CollapseSidebarOverlay'
+        onClick={() => setExpanded(false)}
+      />
+    </>
+  );
+};
 
 export default Nav;
