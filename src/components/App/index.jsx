089e19979fb71df19fba91f0e3708cd7e7563ca2
Kaz Baig <khizermbaig@gmail.com>
2019-06-19 23:08:30 -0500

    react helmet to manage head

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 
 import './App.scss';
 
+import Helmet from '../Helmet';
 import Header from '../Header';
 import Section from '../Section';
 import Menu from '../Menu';
 import Products from '../Products';
 
 const App = () => (
-  <div className='App'>
-    <Header />
-    <Section>
-      <Menu />
-      <Products />
-    </Section>
-  </div>
+  <>
+    <Helmet />
+    <div className='App'>
+      <Header />
+      <Section>
+        <Menu />
+        <Products />
+      </Section>
+    </div>
+  </>
 );
 
 export default App;
