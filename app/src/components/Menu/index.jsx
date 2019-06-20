cbd83aaecae1ca1e27330a765be61cd18092f1af
Kaz Baig <khizermbaig@gmail.com>
2019-06-19 19:05:39 -0500

    header initial styling

--- app/src/components/Menu/index.jsx
+++ app/src/components/Menu/index.jsx
@@ -3,16 +3,14 @@ import React from 'react';
 import './Menu.scss';
 
 const Menu = () => (
-  <nav className='Menu'>
-    <ul>
-      <li>Footwear</li>
-      <li>Water Fountains</li>
-      <li>Garage Door Openers</li>
-      <li>Smart Home Tech</li>
-      <li>Beverages</li>
-      <li>Education</li>
-    </ul>
-  </nav>
+  <ul className='Menu'>
+    <li className='ListItem'>Footwear</li>
+    <li className='ListItem'>Water Fountains</li>
+    <li className='ListItem'>Garage Door Openers</li>
+    <li className='ListItem'>Smart home Tech</li>
+    <li className='ListItem'>Beverages</li>
+    <li className='ListItem'>Education</li>
+  </ul>
 );
 
 export default Menu;
