690aa62efbe94fbbcafbd0a0aa5c6fc169826a54
Kaz Baig <khizermbaig@gmail.com>
2019-06-19 20:08:13 -0500

    menu styles

--- app/src/components/Menu/index.jsx
+++ app/src/components/Menu/index.jsx
@@ -2,14 +2,48 @@ import React from 'react';
 
 import './Menu.scss';
 
-const Menu = () => (
-  <ul className='Menu'>
-    <li className='ListItem'>Footwear</li>
-    <li className='ListItem'>Water Fountains</li>
-    <li className='ListItem'>Garage Door Openers</li>
-    <li className='ListItem'>Smart home Tech</li>
-    <li className='ListItem'>Beverages</li>
-    <li className='ListItem'>Education</li>
+const data = [
+  {
+    id: 1,
+    content: 'Footwear',
+    path: '/footwear'
+  },
+  {
+    id: 2,
+    content: 'Water Fountains',
+    path: '/water-fountains'
+  },
+  {
+    id: 3,
+    content: 'Garage Door Openers',
+    path: '/garage-door-openers'
+  },
+  {
+    id: 4,
+    content: 'Smart Home Tech',
+    path: '/smart-home-tech'
+  },
+  {
+    id: 5,
+    content: 'Beverages',
+    path: '/beverages'
+  },
+  {
+    id: 6,
+    content: 'Education',
+    path: '/education'
+  }
+];
+
+const Menu = ({ ...rest }) => (
+  <ul className='Menu' {...rest}>
+    {data.map(({ id, content, path }) => (
+      <li key={id} className='Menu__ListItem'>
+        <a href={path} className='Menu__ListItemLink'>
+          {content}
+        </a>
+      </li>
+    ))}
   </ul>
 );
 
