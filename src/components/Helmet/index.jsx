cce6d6088956e255b001581429a46c8af8a1431e
Kaz Baig <khizermbaig@gmail.com>
2019-06-21 02:08:44 -0500

    description tag

--- src/components/Helmet/index.jsx
+++ src/components/Helmet/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Helmet as ReactHelmet } from 'react-helmet';
 
-import data from '../../data/head.js';
+import data from '../../data/head';
 
 const Helmet = ({ ...rest }) => (
   <ReactHelmet {...rest}>
     <html lang='en' prefix='og: http://ogp.me/ns#' />
     <title>{data.title}</title>
+    <meta name="description" content={data.description} />
     <link rel='shortcut icon' href={data.favicon} />
   </ReactHelmet>
 );
